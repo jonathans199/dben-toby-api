2e8efe072c06308768513a0df0c30189bd07ae30
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2020-08-12 12:19:30 -0400

    added userController and split out some methods from routes

---
+++ src/controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/userModel')
+
+exports.editUser = (req, res ) => {
+  User.findOne({email: req.body.email})
+  .then(updatedUser => {
+    updatedUser.firstName = req.body.newFirstName
+    updatedUser.lastName = req.body.newLastName
+    updatedUser.userType = req.body.newUserType
+    updatedUser.notes = req.body.newNotes
+    updatedUser.active = req.body.newActive
+    // updatedUser.password = req.body.newPassword
+    return updatedUser.save()
+  })
+  .then(updatedUser => {
+    res.status(200).json(updatedUser)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send('Error: could not edit User')
+  })
+}
